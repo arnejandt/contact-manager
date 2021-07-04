b02c686f044f9eb1e61350393343ad2b52476d9b
Arne Jandt <arneybarneyhtbmail.com>
2021-07-04 18:07:00 +0200

    Contact Class

--- src/main/java/de/htwberlin/Contact.java
+++ src/main/java/de/htwberlin/Contact.java
@@ -5,16 +5,60 @@ public class Contact {
   private final String firstName;
   private final String lastName;
   private final String mobile;
-  private final String street;
-  private final String zipCode;
-  private final String city;
+  private final Address address;
 
-  public Contact(String firstName, String lastName, String mobile, String street, String zipCode, String city) {
+
+
+
+  public Contact(String firstName, String lastName, String mobile, Address address) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.mobile = mobile;
-    this.street = street;
-    this.zipCode = zipCode;
-    this.city = city;
+    this.address = address;
+
   }
+  public Contact(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.mobile = null;
+    this.address = null;
+
+  }
+
+  public Contact(String firstName, String lastName,Address address) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.mobile = null;
+    this.address = address;
+
+  }
+
+  public Contact(String firstName, String lastName,String mobile) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.mobile = mobile;
+    this.address = null;
+
+  }
+
+  public String toString(){
+    if (address==null&& mobile==null){
+      return firstName + lastName + ", n/a, " + " n/a ";
+    }
+
+    if (address==null){
+      return firstName + lastName + mobile + " n/a";
+    }
+
+    if (mobile==null){
+      return firstName + lastName + " n/a " + address;
+    }
+
+    else {
+      return firstName + lastName + mobile + address;
+    }
+
+  }
+
+
 }

f9dbd181e7b9f31455c46ae2685ced5da89aa2c0
Arne Jandt <arneybarneyhtbmail.com>
2021-07-04 18:05:38 +0200

    Application

--- src/main/java/de/htwberlin/App.java
+++ src/main/java/de/htwberlin/App.java
@@ -3,14 +3,14 @@ package de.htwberlin;
 public class App {
 
   public static void main(String[] args) {
-    // Contacts of Alice
-//    ContactBook.addContact(new Contact("Megan", "Gilmore"));
-//    ContactBook.addContact(new Contact("Harry", "Fisher", "+491253490723"));
+    //Contacts of Alice
+   ContactBook.addContact(new Contact("Megan", "Gilmore"));
+    ContactBook.addContact(new Contact("Harry", "Fisher", "+491253490723"));
 
     // Contacts of Bob
-//    ContactBook.addContact(new Contact("Robert", "White", new Address("Warschauer Str. 1", "12345", "Berlin")));
-//    ContactBook.addContact(new Contact("Mary", "Stuart", "+491239730943", new Address("Warschauer Str. 2", "12345", "Berlin")));
-//    ContactBook.addContact(new Contact("Bobby", "Martinez"));
+   ContactBook.addContact(new Contact("Robert", "White", new Address("Warschauer Str. 1", "12345", "Berlin")));
+   ContactBook.addContact(new Contact("Mary", "Stuart", "+491239730943", new Address("Warschauer Str. 2", "12345", "Berlin")));
+   ContactBook.addContact(new Contact("Bobby", "Martinez"));
 
     ContactBook.showContacts();
   }
